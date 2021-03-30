b4bdb2a98566e51c5263b3af790fadf817890277
tasya <tasya00661@gmail.com>
2021-03-30 15:53:19 +0700

    Migration

---
+++ database/migrations/2021_03_30_010522_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_barang', 30)->nullable();
+            $table->string('nama_barang', 50)->nullable();
+            $table->string('kategori_barang', 50)->nullable();
+            $table->string('harga', 20)->nullable();
+            $table->string('qty', 20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
